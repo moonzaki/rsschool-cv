ff324b91c3a4e67ff0a9678315998a68108d0d35
moonzaki <artmagaziine@gmail.com>
2022-06-07 01:31:35 +0300

    fix: full change index.html, style.css, upload new image to avatar, update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 function changeColor(){
-    let h1 = document.querySelector( '.title' );
+    let h1 = document.querySelector( '.profile-title' );
     h1.innerHTML = h1.innerHTML.replace( /(\w+)$/i, '<span class="last-word">$&</span>' );
     let h2 = document.querySelectorAll( '.section-title' );
-    [...h2].filter((e, i) => (i % 2)).forEach(e => e.innerHTML = e.innerHTML.replace( /(\w+)$/i, '<span class="sect-word">$&</span>'));
+    [...h2].filter((e, i) => (i % 2 == 0)).forEach(e => e.innerHTML = e.innerHTML.replace( /(\w+)$/i, '<span class="section-title-word">$&</span>'));
 }   
 changeColor();
