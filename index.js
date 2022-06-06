ecd5be859ccc0310c0591677c2cd5f93d42b67ec
moonzaki <artmagaziine@gmail.com>
2022-06-06 16:07:51 +0300

    feat: add new functional to index.js, update content index.html, update style all blocks in style.css

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 function changeColor(){
-    let a = document.querySelector( '.title' );
-    a.innerHTML = a.innerHTML.replace( /(\w+)$/i, '<span class="last-word">$&</span>' );
-   
-}
+    let h1 = document.querySelector( '.title' );
+    h1.innerHTML = h1.innerHTML.replace( /(\w+)$/i, '<span class="last-word">$&</span>' );
+    let h2 = document.querySelectorAll( '.section-title' );
+    [...h2].filter((e, i) => (i % 2)).forEach(e => e.innerHTML = e.innerHTML.replace( /(\w+)$/i, '<span class="sect-word">$&</span>'));
+}   
 changeColor();
