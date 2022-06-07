8ccc59e355641a18d2bc120f0af8038b52e0d087
moonzaki <artmagaziine@gmail.com>
2022-06-07 23:29:21 +0300

    feat: add toggle and clode menu functions to index.js, add menu to index.html and style.css, change indents in all documets

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function changeColor(){
     [...h2].filter((e, i) => (i % 2 == 0)).forEach(e => e.innerHTML = e.innerHTML.replace( /(\w+)$/i, '<span class="section-title-word">$&</span>'));
 }   
 changeColor();
+
+function toggleClass(){
+    let nav = document.querySelector('.nav'); 
+    document.querySelector('.toggle').addEventListener('click', () => {
+        nav.classList.toggle('nav-show');
+    });
+}
+toggleClass();
+
+function closeNav(){
+    let nav = document.querySelector('.nav'); 
+    let li = document.querySelectorAll('.n-item');
+    li.forEach( el => {
+        el.addEventListener('click', () => {
+            setTimeout(() => {
+            nav.classList.remove('nav-show');
+            }, 1000);
+        });
+    });
+}
+closeNav();
